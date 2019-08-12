be5833f0b56eef3e3650c6beb1e5c3378f1053f9
Ashley <ashjohnson318@gmail.com>
2019-08-12 15:21:29 +0000

    Headline db added

--- db/migrate/20190803002751_create_headlines.rb
+++ db/migrate/20190803002751_create_headlines.rb
@@ -1,7 +1,8 @@
 class CreateHeadlines < ActiveRecord::Migration[5.2]
   def change
     create_table :headlines do |t|
-    
+    	t.string :main 
+    	t.string :byline
 
       t.timestamps
     end
